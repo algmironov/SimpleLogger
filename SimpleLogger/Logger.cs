ecaa622d836cd2065346121dfbb12d679e27962d
algmironov <al.g.mironov@gmail.com>
2024-07-14 23:35:38 +0300

    - code improvements

--- SimpleLogger/Logger.cs
+++ SimpleLogger/Logger.cs
@@ -1,12 +1,17 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.Runtime.CompilerServices;
+﻿using System.Runtime.CompilerServices;
 
 namespace SimpleLogger
 {
+    /// <summary>
+    /// Provides logging functionality with different log levels.
+    /// </summary>
     public class Logger
     {
         private string _folder;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Logger"/> class.
+        /// </summary>
         public Logger()
         {
             var appDirectory = AppDomain.CurrentDomain.BaseDirectory;
@@ -18,6 +23,13 @@ namespace SimpleLogger
             }
         }
 
+        /// <summary>
+        /// Logs a message with additional information about the source of the log.
+        /// </summary>
+        /// <param name="message">The log message.</param>
+        /// <param name="memberName">The name of the calling method. This is automatically set by the compiler.</param>
+        /// <param name="filePath">The file path of the source code. This is automatically set by the compiler.</param>
+        /// <param name="lineNumber">The line number in the source code. This is automatically set by the compiler.</param>
         public void Log(string message,
                          [CallerMemberName] string memberName = "",
                          [CallerFilePath] string filePath = "",
@@ -25,8 +37,9 @@ namespace SimpleLogger
         {
             try
             {
+                var source = filePath.Split('\\').Last();
                 var file = GetCurrentLogFile();
-                var logMessage = $"{GetCurrentTime()} {message} at method: {memberName} in file: {filePath} at line: {lineNumber};\n";
+                var logMessage = $"[{GetCurrentTime()}] {message} --- file: {source} --- method: {memberName} --- line: {lineNumber};\n";
                 File.AppendAllTextAsync(file, logMessage);
                 Console.WriteLine(logMessage);
             }
@@ -36,40 +49,93 @@ namespace SimpleLogger
             }
         }
 
+        /// <summary>
+        /// Logs an informational message.
+        /// </summary>
+        /// <param name="message">The log message.</param>
+        /// <param name="memberName">The name of the calling method. This is automatically set by the compiler.</param>
+        /// <param name="filePath">The file path of the source code. This is automatically set by the compiler.</param>
+        /// <param name="lineNumber">The line number in the source code. This is automatically set by the compiler.</param>
         public void Info(string message, [CallerMemberName] string memberName = "", [CallerFilePath] string filePath = "", [CallerLineNumber] int lineNumber = 0)
         {
             var infoMessage = $"INFO: {message}";
             Log(infoMessage, memberName, filePath, lineNumber);
         }
 
+        /// <summary>
+        /// Logs an informational message.
+        /// </summary>
+        /// <param name="message">The log message.</param>
+        /// <param name="memberName">The name of the calling method. This is automatically set by the compiler.</param>
+        /// <param name="filePath">The file path of the source code. This is automatically set by the compiler.</param>
+        /// <param name="lineNumber">The line number in the source code. This is automatically set by the compiler.</param>
+
         public void Warn(string message, [CallerMemberName] string memberName = "", [CallerFilePath] string filePath = "", [CallerLineNumber] int lineNumber = 0)
         {
-            var infoMessage = $"WARN: {GetCurrentTime()} {message}";
+            var infoMessage = $"WARN: {message}";
             Log(infoMessage, memberName, filePath, lineNumber);
         }
 
-        public void Error(string message, [CallerMemberName] string memberName = "", [CallerFilePath] string filePath = "", [CallerLineNumber] int lineNumber = 0)
+        /// <summary>
+        /// Logs an informational message.
+        /// </summary>
+        /// <param name="message">The log message.</param>
+        /// <param name="memberName">The name of the calling method. This is automatically set by the compiler.</param>
+        /// <param name="filePath">The file path of the source code. This is automatically set by the compiler.</param>
+        /// <param name="lineNumber">The line number in the source code. This is automatically set by the compiler.</param>
+
+        public void Error(string message, Exception ex, [CallerMemberName] string memberName = "", [CallerFilePath] string filePath = "", [CallerLineNumber] int lineNumber = 0)
         {
-            var infoMessage = $"ERROR: {GetCurrentTime()} {message}";
-            Log(infoMessage, memberName, filePath, lineNumber);
+            try
+            {
+                var errorMessage = $"ERROR: {message} at : {ex.StackTrace.Split(':').Last()}";
+                Log(errorMessage, memberName, filePath, lineNumber);
+            }
+            catch (Exception)
+            {
+                var errorMessage = $"ERROR: {message}";
+                Log(errorMessage, memberName, filePath, lineNumber);
+            }
+            
         }
+
+        /// <summary>
+        /// Logs an informational message.
+        /// </summary>
+        /// <param name="message">The log message.</param>
+        /// <param name="memberName">The name of the calling method. This is automatically set by the compiler.</param>
+        /// <param name="filePath">The file path of the source code. This is automatically set by the compiler.</param>
+        /// <param name="lineNumber">The line number in the source code. This is automatically set by the compiler.</param>
 
         public void Fatal(string message, [CallerMemberName] string memberName = "", [CallerFilePath] string filePath = "", [CallerLineNumber] int lineNumber = 0)
         {
-            var infoMessage = $"FATAL: {GetCurrentTime()} {message}";
+            var infoMessage = $"FATAL: {message}";
             Log(infoMessage, memberName, filePath, lineNumber);
         }
+
+        /// <summary>
+        /// Logs an informational message.
+        /// </summary>
+        /// <param name="message">The log message.</param>
+        /// <param name="memberName">The name of the calling method. This is automatically set by the compiler.</param>
+        /// <param name="filePath">The file path of the source code. This is automatically set by the compiler.</param>
+        /// <param name="lineNumber">The line number in the source code. This is automatically set by the compiler.</param>
 
         public void Debug(string message, [CallerMemberName] string memberName = "", [CallerFilePath] string filePath = "", [CallerLineNumber] int lineNumber = 0)
         {
-            var infoMessage = $"DEBUG: {GetCurrentTime()} {message}";
+            var infoMessage = $"DEBUG: {message}";
             Log(infoMessage, memberName, filePath, lineNumber);
         }
 
+        /// <summary>
+        /// Gets the current log file based on the date.
+        /// </summary>
+        /// <returns>The path to the current log file.</returns>
+
         private string GetCurrentLogFile()
         {
-            var currentDate = DateTime.Now.ToString("ddMMyyyy");
-            var fileName = $"{currentDate}.txt";
+            var currentDate = DateTime.Now.ToString("dd-MM-yyyy");
+            var fileName = $"{currentDate}.log";
             var filePath = Path.Combine(_folder, fileName);
             if (File.Exists(filePath))
             {
@@ -78,6 +144,11 @@ namespace SimpleLogger
             File.Create(filePath);
             return filePath;
         }
+
+        /// <summary>
+        /// Gets the current time in the format "yyyy-MM-dd HH:mm:ss:fff".
+        /// </summary>
+        /// <returns>The current time as a string.</returns>
 
         private string GetCurrentTime()
         {
