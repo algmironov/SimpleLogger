7f5f8efdce9662757158794f415c2147177b1df8
algmironov <al.g.mironov@gmail.com>
2024-07-14 18:57:17 +0300

    Добавьте файлы проекта.

---
+++ LoggerTestApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+using SimpleLogger;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        var logger = new Logger();
+
+        logger.Log("Просто Log");
+        logger.Info("just Info message");
+        logger.Warn("just WARN message");
+        logger.Error("just ERROR message");
+        logger.Log("1Просто Log");
+        logger.Info("just Info message");
+        logger.Warn("just WARN message");
+        logger.Error("just ERROR message");
+
+        logger.Info("Application started successfully.");
+        logger.Warn("Low disk space on drive C:.");
+        try
+        {
+            throw new NullReferenceException("Example exception");
+        }
+        catch (Exception ex)
+        {
+            logger.Error(ex.Message);
+        }
+        logger.Debug("Entering method Main with parameters: args");
+
+    }
+}
+
---
+++ SimpleLogger/Logger.cs
@@ -0,0 +1,88 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+
+namespace SimpleLogger
+{
+    public class Logger
+    {
+        private string _folder;
+
+        public Logger()
+        {
+            var appDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            _folder = Path.Combine(appDirectory, "Logs");
+
+            if (!Directory.Exists(_folder))
+            {
+                Directory.CreateDirectory(_folder);
+            }
+        }
+
+        public void Log(string message,
+                         [CallerMemberName] string memberName = "",
+                         [CallerFilePath] string filePath = "",
+                         [CallerLineNumber] int lineNumber = 0)
+        {
+            try
+            {
+                var file = GetCurrentLogFile();
+                var logMessage = $"{GetCurrentTime()} {message} at method: {memberName} in file: {filePath} at line: {lineNumber};\n";
+                File.AppendAllTextAsync(file, logMessage);
+                Console.WriteLine(logMessage);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An exception {ex} occured during log writing to file");
+            }
+        }
+
+        public void Info(string message, [CallerMemberName] string memberName = "", [CallerFilePath] string filePath = "", [CallerLineNumber] int lineNumber = 0)
+        {
+            var infoMessage = $"INFO: {message}";
+            Log(infoMessage, memberName, filePath, lineNumber);
+        }
+
+        public void Warn(string message, [CallerMemberName] string memberName = "", [CallerFilePath] string filePath = "", [CallerLineNumber] int lineNumber = 0)
+        {
+            var infoMessage = $"WARN: {GetCurrentTime()} {message}";
+            Log(infoMessage, memberName, filePath, lineNumber);
+        }
+
+        public void Error(string message, [CallerMemberName] string memberName = "", [CallerFilePath] string filePath = "", [CallerLineNumber] int lineNumber = 0)
+        {
+            var infoMessage = $"ERROR: {GetCurrentTime()} {message}";
+            Log(infoMessage, memberName, filePath, lineNumber);
+        }
+
+        public void Fatal(string message, [CallerMemberName] string memberName = "", [CallerFilePath] string filePath = "", [CallerLineNumber] int lineNumber = 0)
+        {
+            var infoMessage = $"FATAL: {GetCurrentTime()} {message}";
+            Log(infoMessage, memberName, filePath, lineNumber);
+        }
+
+        public void Debug(string message, [CallerMemberName] string memberName = "", [CallerFilePath] string filePath = "", [CallerLineNumber] int lineNumber = 0)
+        {
+            var infoMessage = $"DEBUG: {GetCurrentTime()} {message}";
+            Log(infoMessage, memberName, filePath, lineNumber);
+        }
+
+        private string GetCurrentLogFile()
+        {
+            var currentDate = DateTime.Now.ToString("ddMMyyyy");
+            var fileName = $"{currentDate}.txt";
+            var filePath = Path.Combine(_folder, fileName);
+            if (File.Exists(filePath))
+            {
+                return filePath;
+            }
+            File.Create(filePath);
+            return filePath;
+        }
+
+        private string GetCurrentTime()
+        {
+            return DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss:fff");
+        }
+
+    }
+}
